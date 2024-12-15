c5f20a36f71f138445df3b009b6a9c6023ddcc39
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2024-12-15 15:31:24 +0530

    Add files via upload

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Loading.css'
+
+
+
+const Loading = () => {
+  return (
+	
+	
+	<>
+	
+	<div class="banter-loader">
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+  <div class="banter-loader__box"></div>
+
+
+</div>
+
+
+
+	</>
+  )
+}
+
+export default Loading
+
+
