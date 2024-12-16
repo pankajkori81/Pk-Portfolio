12494032a8f6e3434c6587857ee4e1a47d0c7956
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2024-12-16 12:23:03 +0530

    Update Education.jsx

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,16 +1,65 @@
-import React from 'react'
-import './Education.css';
+import React, { useEffect } from "react";
+import "./Education.css";
+
+import AOS from "aos";
+import 'aos/dist/aos.css';
+
 
 
 const Education = () => {
-  return (
-  
-    <>
-    
-    
-    
-    </>
-  )
-}
 
-export default Education
+
+  useEffect(()=>{
+
+    AOS.init({
+      offset:200,
+      duration:900
+    })
+
+  })
+
+
+  return (
+    <>
+      <div className="education">
+        <h1 data-aos="flip-right" >Education</h1>
+
+        <div className="edu-data" data-aos="zoom-in-up" >
+          <div className="sub-datas">
+            <img src="/atom.png" alt="Education" />
+          </div>
+
+          <div className="sub-datas">
+            <p> A.C Patil College of Engg. University of Mumbai </p>
+            <p>Course : B.E in Computer Engineering</p>
+
+          </div>
+
+          <div className="sub-datas">
+            <p>Academic Years : 2021-2025</p>
+
+          </div>
+        </div>
+
+        <div className="edu-data" data-aos="zoom-in-up" >
+          <div className="sub-datas">
+            <img src="/atom.png" alt="Education" />
+          </div>
+
+          <div className="sub-datas">
+            <p>Kirti College.  </p>
+            <p>Senior Secondary Education</p>
+
+          </div>
+
+          <div className="sub-datas">
+          <p>Academic Years : 2018-2020</p>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Education;
